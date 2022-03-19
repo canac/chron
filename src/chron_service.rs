b896d438cc717ea9b17357c59231b4360d5d4a7d
Caleb Cox <canac@users.noreply.github.com>
2022-03-19 10:30:30 -0500

    Add web server

--- src/chron_service.rs
+++ src/chron_service.rs
@@ -1,6 +1,6 @@
-use crate::job_scheduler::{Job, JobScheduler};
 use crate::schema::run;
 use anyhow::{anyhow, bail, Context, Result};
+use chrono::{DateTime, Utc};
 use cron::Schedule;
 use diesel::prelude::*;
 use diesel::SqliteConnection;
@@ -13,6 +13,7 @@ use std::process::{self, Child};
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Duration;
 
 embed_migrations!();
 
@@ -22,17 +23,53 @@ no_arg_sql_function!(
     "Represents the SQL last_insert_row() function"
 );
 
-pub struct ChronService<'job> {
-    log_dir: PathBuf,
-    scheduled_jobs: HashMap<String, Job<'job>>,
-    // The key is the name and the value is the command to run
-    startup_commands: HashMap<String, String>,
-    scheduler: JobScheduler<'job>,
-    running_commands: Arc<Mutex<HashMap<String, Arc<Mutex<Child>>>>>,
-    db_connection: Arc<Mutex<SqliteConnection>>,
+struct ScheduledCommand {
+    schedule: Schedule,
+    last_tick: Option<DateTime<Utc>>,
 }
 
-impl<'job> ChronService<'job> {
+impl ScheduledCommand {
+    fn tick(&mut self) -> bool {
+        let now = Utc::now();
+        let should_run = match self.last_tick {
+            None => false,
+            Some(last_tick) => {
+                if let Some(event) = self.schedule.after(&last_tick).next() {
+                    event <= now
+                } else {
+                    false
+                }
+            }
+        };
+        self.last_tick = Some(now);
+        should_run
+    }
+}
+
+enum CommandType {
+    Startup,
+    Scheduled(Box<ScheduledCommand>),
+}
+
+struct Command {
+    name: String,
+    command: String,
+    log_path: PathBuf,
+    process: Option<Child>,
+    command_type: CommandType,
+}
+
+#[derive(Clone)]
+struct ThreadState {
+    db_connection: Arc<Mutex<SqliteConnection>>,
+    commands: HashMap<String, Arc<Mutex<Command>>>,
+}
+pub struct ChronService {
+    log_dir: PathBuf,
+    state: ThreadState,
+}
+
+impl ChronService {
     // Create a new ChronService instance
     pub fn new(chron_dir: &Path) -> Result<Self> {
         let db_path = chron_dir.join("db.sqlite");
@@ -40,13 +77,13 @@ impl<'job> ChronService<'job> {
             .with_context(|| format!("Error opening SQLite database {db_path:?}"))?;
         embedded_migrations::run(&connection).context("Error running SQLite migrations")?;
 
+        let thread_state = ThreadState {
+            db_connection: Arc::new(Mutex::new(connection)),
+            commands: HashMap::new(),
+        };
         Ok(ChronService {
             log_dir: chron_dir.join("logs"),
-            scheduled_jobs: HashMap::new(),
-            startup_commands: HashMap::new(),
-            scheduler: JobScheduler::new(),
-            running_commands: Arc::new(Mutex::new(HashMap::new())),
-            db_connection: Arc::new(Mutex::new(connection)),
+            state: thread_state,
         })
     }
 
@@ -56,11 +93,20 @@ impl<'job> ChronService<'job> {
             bail!("Invalid command name {name}")
         }
 
-        if self.startup_commands.contains_key(name) {
+        if self.state.commands.contains_key(name) {
             bail!("A job with the name {name} already exists")
         }
-        self.startup_commands
-            .insert(name.to_string(), command.to_string());
+
+        let command = Command {
+            name: name.to_string(),
+            command: command.to_string(),
+            log_path: self.calculate_log_path(name),
+            process: None,
+            command_type: CommandType::Startup,
+        };
+        self.state
+            .commands
+            .insert(name.to_string(), Arc::new(Mutex::new(command)));
 
         Ok(())
     }
@@ -68,97 +114,112 @@ impl<'job> ChronService<'job> {
     // Add a new command to be run on the given schedule
     pub fn schedule<'cmd>(
         &mut self,
-        name: &'job str,
+        name: &str,
         schedule_expression: &str,
         command: &'cmd str,
-    ) -> Result<()>
-    where
-        'cmd: 'job,
-    {
+    ) -> Result<()> {
         if !Self::validate_name(name) {
             bail!("Invalid command name {name}")
         }
 
-        if self.scheduled_jobs.contains_key(name) {
+        if self.state.commands.contains_key(name) {
             bail!("A job with the name {name} already exists")
         }
 
         let schedule = Schedule::from_str(schedule_expression).with_context(|| {
             format!("Failed to parse schedule expression {schedule_expression}")
         })?;
-        let (db_connection, running_commands, log_dir) = (
-            self.db_connection.clone(),
-            self.running_commands.clone(),
-            self.log_dir.clone(),
-        );
-        let job = Job::new(schedule, move || {
-            let (db_connection, running_commands, log_dir, name, command) = (
-                db_connection.clone(),
-                running_commands.clone(),
-                log_dir.clone(),
-                name.to_string(),
-                command.to_string(),
-            );
-            thread::spawn(move || {
-                Self::exec_command(&db_connection, &running_commands, &log_dir, &name, &command)
-                    .unwrap()
-            });
-        });
-        self.scheduler.add(job);
-        // self.scheduled_jobs.insert(name.to_string(), job);
+        let command = Command {
+            name: name.to_string(),
+            command: command.to_string(),
+            log_path: self.calculate_log_path(name),
+            process: None,
+            command_type: CommandType::Scheduled(Box::new(ScheduledCommand {
+                schedule,
+                last_tick: None,
+            })),
+        };
+        self.state
+            .commands
+            .insert(name.to_string(), Arc::new(Mutex::new(command)));
 
         Ok(())
     }
 
+    pub async fn start_server(&self) -> Result<(), std::io::Error> {
+        use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+        async fn hello(data: web::Data<ThreadState>) -> impl Responder {
+            HttpResponse::Ok().body(data.commands.len().to_string())
+        }
+
+        let app_data = self.state.clone();
+        HttpServer::new(move || {
+            let app_data = web::Data::new(app_data.clone());
+            App::new()
+                .app_data(app_data)
+                .route("/", web::get().to(hello))
+        })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+    }
+
     // Start the chron service, running all scripts on their specified interval
     // Note that this function starts an infinite loop and will never return
-    pub fn run(mut self) -> Result<()> {
+    pub fn run(&self) -> Result<()> {
         // Run all of the startup scripts
-        for (name, command) in self.startup_commands.into_iter() {
-            let (db_connection, running_commands, log_dir) = (
-                self.db_connection.clone(),
-                self.running_commands.clone(),
-                self.log_dir.clone(),
-            );
+        for command in self.state.commands.values() {
+            let thread_state = self.state.clone();
+            let command = command.clone();
             thread::spawn(move || loop {
-                Self::exec_command(
-                    &db_connection,
-                    &running_commands,
-                    &log_dir,
-                    name.as_str(),
-                    command.as_str(),
-                )
-                .unwrap();
+                let mut command_guard = command.lock().unwrap();
+                match &mut command_guard.command_type {
+                    CommandType::Startup => {
+                        drop(command_guard);
+                        Self::exec_command(&thread_state, &command).unwrap();
 
-                // Re-run the command after a few seconds
-                thread::sleep(std::time::Duration::from_secs(3));
+                        // Re-run the command after a few seconds
+                        thread::sleep(Duration::from_secs(3));
+                    }
+                    CommandType::Scheduled(scheduled_command) => {
+                        if scheduled_command.tick() {
+                            drop(command_guard);
+                            Self::exec_command(&thread_state, &command).unwrap();
+                            thread::sleep(Duration::from_millis(500));
+                        }
+                    }
+                }
             });
         }
 
-        // Run the scheduled scripts
-        loop {
-            self.scheduler.tick();
-            thread::sleep(self.scheduler.time_till_next_job());
-        }
+        Ok(())
     }
 
     // Kill a process by its name
     pub fn kill_by_name(&self, name: &str) -> Result<()> {
-        let running_commands = self.running_commands.lock().unwrap();
-        let child = running_commands
-            .get(name)
-            .ok_or_else(|| anyhow!("No process with name {name}"))?;
-        child.lock().unwrap().kill()?;
+        let mut command = self
+            .state
+            .commands
+            .get(&name.to_string())
+            .ok_or_else(|| anyhow!("No process with name {name}"))?
+            .lock()
+            .unwrap();
+        if let Some(process) = command.process.as_mut() {
+            process.kill()?;
+        }
         Ok(())
     }
 
     // Lookup the process id of a job by its name
     pub fn pid_from_name(&self, name: &str) -> Option<u32> {
-        self.running_commands
-            .lock()
-            .unwrap()
-            .get(name)
-            .map(|child| child.lock().unwrap().id())
+        self.state
+            .commands
+            .get(&name.to_string())
+            .and_then(|child| {
+                let child = child.lock().unwrap();
+                child.process.as_ref().map(|process| process.id())
+            })
     }
 
     // Helper to validate the command name
@@ -170,23 +231,28 @@ impl<'job> ChronService<'job> {
         RE.is_match(name)
     }
 
+    // Helper to get the log file path for a command
+    fn calculate_log_path(&self, name: &str) -> PathBuf {
+        let mut log_path = self.log_dir.join(name);
+        log_path.set_extension("log");
+        log_path
+    }
+
     // Helper to execute the specified command
-    fn exec_command(
-        db_connection: &Arc<Mutex<SqliteConnection>>,
-        running_commands: &Arc<Mutex<HashMap<String, Arc<Mutex<Child>>>>>,
-        log_dir: &Path,
-        name: &str,
-        command: &str,
-    ) -> Result<()> {
+    fn exec_command(thread_state: &ThreadState, command_mutex: &Arc<Mutex<Command>>) -> Result<()> {
         let start_time = chrono::Local::now();
         let formatted_start_time = start_time.to_rfc3339();
 
-        println!("{formatted_start_time} Running {name}: {command}");
+        let mut command = command_mutex.lock().unwrap();
+        println!(
+            "{formatted_start_time} Running {}: {}",
+            command.name, command.command
+        );
 
         // Record the run in the database
-        let connection = db_connection.lock().unwrap();
+        let connection = thread_state.db_connection.lock().unwrap();
         diesel::insert_into(run::table)
-            .values(run::dsl::name.eq(name))
+            .values(run::dsl::name.eq(command.name.clone()))
             .execute(&*connection)
             .context("Error saving run to database")?;
         let run_id = diesel::select(last_insert_rowid)
@@ -195,15 +261,19 @@ impl<'job> ChronService<'job> {
         drop(connection);
 
         // Open the log file, creating the directory if necessary
+        let log_dir = command.log_path.parent().with_context(|| {
+            format!(
+                "Failed to get parent dir of log file {:?}",
+                command.log_path
+            )
+        })?;
         fs::create_dir_all(log_dir)
             .with_context(|| format!("Failed to create log dir {log_dir:?}"))?;
-        let mut log_path = log_dir.join(name);
-        log_path.set_extension("log");
         let mut log_file = fs::OpenOptions::new()
             .create(true)
             .append(true)
-            .open(log_path.clone())
-            .with_context(|| format!("Failed to open log file {log_path:?}"))?;
+            .open(command.log_path.clone())
+            .with_context(|| format!("Failed to open log file {:?}", command.log_path))?;
 
         // Write the log file header for this execution
         let divider: &'static str = "----------------------------------------------------------------------------------------------------";
@@ -211,22 +281,42 @@ impl<'job> ChronService<'job> {
 
         // Run the command
         let clone_log_file = || log_file.try_clone().context("Failed to clone log file");
-        let process = Arc::new(Mutex::new(
-            process::Command::new("sh")
-                .args(["-c", command])
-                .stdin(process::Stdio::null())
-                .stdout(clone_log_file()?)
-                .stderr(clone_log_file()?)
-                .spawn()
-                .with_context(|| format!("Failed to run command {command}"))?,
-        ));
-        (*running_commands.lock().unwrap()).insert(name.to_string(), process.clone());
-        let status = process
-            .lock()
-            .unwrap()
-            .wait()
-            .with_context(|| format!("Failed to get command status for command {command}"))?;
-        (*running_commands.lock().unwrap()).remove(name);
+        let process = process::Command::new("sh")
+            .args(["-c", &command.command])
+            .stdin(process::Stdio::null())
+            .stdout(clone_log_file()?)
+            .stderr(clone_log_file()?)
+            .spawn()
+            .with_context(|| format!("Failed to run command {}", command.command))?;
+
+        command.process = Some(process);
+        drop(command);
+
+        // Check the status every second until it exists without holding onto the child process mutex
+        let status = loop {
+            let mut command = command_mutex.lock().unwrap();
+            let maybe_status = command
+                .process
+                .as_mut()
+                .unwrap()
+                .try_wait()
+                .with_context(|| {
+                    format!(
+                        "Failed to get command status for command {}",
+                        command.command
+                    )
+                })?;
+            match maybe_status {
+                None => std::thread::sleep(Duration::from_millis(1000)),
+                Some(status) => {
+                    break status;
+                }
+            }
+        };
+
+        let mut command = command_mutex.lock().unwrap();
+        command.process = None;
+        drop(command);
 
         // Write the log file footer that contains the execution status
         let status_code = match status.code() {
@@ -239,7 +329,7 @@ impl<'job> ChronService<'job> {
         if let Some(code) = status.code() {
             diesel::update(run::table.find(run_id))
                 .set(run::dsl::status_code.eq(code))
-                .execute(&*db_connection.lock().unwrap())
+                .execute(&*thread_state.db_connection.lock().unwrap())
                 .context("Error updating run status in the database")?;
         }
 
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,22 @@ extern crate diesel;
 extern crate diesel_migrations;
 
 mod chron_service;
-mod job_scheduler;
 mod run;
 mod schema;
 
 use crate::chron_service::ChronService;
 use anyhow::{Context, Result};
 
-fn main() -> Result<()> {
+#[actix_web::main]
+async fn main() -> Result<()> {
     let project_dirs = directories::ProjectDirs::from("com", "canac", "chron")
         .context("Failed to determine application directories")?;
     let mut chron = ChronService::new(project_dirs.data_local_dir())?;
     chron.startup("startup", "echo 'Startup'")?;
-    chron.schedule("echo", "* * * * * * *", "echo 'Hello world!'")?;
+    chron.schedule("echo", "0 * * * * * *", "sleep 1; echo 'Hello world!'")?;
+    chron.schedule("error", "* * * * * * *", "echo 'Hello world!'; exit 1;")?;
     chron.run()?;
+    chron.start_server().await?;
 
     Ok(())
 }
