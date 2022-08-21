0dc8ebd020ff3b1c5a571621af8b3d1501c9613b
Caleb Cox <canac@users.noreply.github.com>
2022-08-21 16:58:48 -0500

    Split use

--- build.rs
+++ build.rs
@@ -3,11 +3,9 @@ mod cli;
 
 use clap::CommandFactory;
 use cli::Cli;
-use std::{
-    fs::{create_dir_all, write},
-    io::Result,
-    path::PathBuf,
-};
+use std::fs::{create_dir_all, write};
+use std::io::Result;
+use std::path::PathBuf;
 
 fn main() {
     // Don't rebuild when the generated completions change
