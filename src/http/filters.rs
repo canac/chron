5ba84b1a55ea5a147d36225681623fcfa0ad2233
Caleb Cox <canac@users.noreply.github.com>
2025-05-05 21:26:42 -0500

    Upgrade askama

--- src/http/filters.rs
+++ src/http/filters.rs
@@ -1,20 +1,20 @@
 #![allow(clippy::trivially_copy_pass_by_ref, clippy::unnecessary_wraps)]
 
 use crate::format;
-use askama::Result;
+use askama::{Result, Values};
 use chrono::{DateTime, Duration, Local};
 
 // Format a duration as a human-readable string
-pub fn duration(duration: &&Duration) -> Result<String> {
+pub fn duration(duration: &&Duration, _: &dyn Values) -> Result<String> {
     Ok(crate::format::duration(duration))
 }
 
 // Format a date as text
-pub fn date(date: &&DateTime<Local>) -> Result<String> {
+pub fn date(date: &&DateTime<Local>, _: &dyn Values) -> Result<String> {
     Ok(date.format("%a %h %d, %Y %r").to_string())
 }
 
 // Format a date relative to now
-pub fn relative_date(date: &&DateTime<Local>) -> Result<String> {
+pub fn relative_date(date: &&DateTime<Local>, _: &dyn Values) -> Result<String> {
     Ok(format::relative_date(date))
 }
