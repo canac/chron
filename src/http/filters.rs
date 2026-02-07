4e10899f470f9ca32a546ba9d045a8a913c81f1a
Caleb Cox <canac@users.noreply.github.com>
2026-02-07 07:16:17 -0600

    Upgrade dependencies

--- src/http/filters.rs
+++ src/http/filters.rs
@@ -1,15 +1,22 @@
-#![allow(clippy::trivially_copy_pass_by_ref, clippy::unnecessary_wraps)]
+#![allow(
+    clippy::inline_always,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::unnecessary_wraps,
+    clippy::unused_self
+)]
 
 use crate::format;
 use askama::{Result, Values};
 use chrono::{DateTime, Duration, Local, TimeZone};
 
 /// Format a duration as a human-readable string
+#[askama::filter_fn]
 pub fn duration(duration: &&Duration, _: &dyn Values) -> Result<String> {
     Ok(crate::format::duration(duration))
 }
 
 /// Format a date as text
+#[askama::filter_fn]
 pub fn date<Tz>(date: &&DateTime<Tz>, _: &dyn Values) -> Result<String>
 where
     Tz: TimeZone,
@@ -22,6 +29,7 @@ where
 }
 
 /// Format a date relative to now
+#[askama::filter_fn]
 pub fn relative_date<Tz>(date: &&DateTime<Tz>, _: &dyn Values) -> Result<String>
 where
     Tz: TimeZone,
