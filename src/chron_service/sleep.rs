a336243cf341a41a3ae64fbeddf8fe7bd2ee7dd2
Caleb Cox <canac@users.noreply.github.com>
2026-08-25 08:35:42 -0500

    Sleep for at most one minute

--- src/chron_service/sleep.rs
+++ src/chron_service/sleep.rs
@@ -4,7 +4,7 @@ use tokio::time::sleep;
 
 /// Sleep until the specified timestamp, preventing oversleeping during hibernation
 pub async fn sleep_until(timestamp: DateTime<Utc>) {
-    let max_sleep = Duration::from_secs(1);
+    let max_sleep = Duration::from_secs(60);
     // to_std returns Err if the duration is negative, in which case we have hit the timestamp and can stop looping
     while let Ok(duration) = timestamp.signed_duration_since(Utc::now()).to_std() {
         // Sleep for a maximum of one minute to prevent oversleeping when the computer hibernates
